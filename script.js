37de8f021b6d1eadf1598d3176e43f7d32adff76
SunFlyMan <frickdiscord321@gmail.com>
2025-04-26 14:24:02 +0100

    menu

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ menu.addEventListener("click", function(event){
     if (clickTarget.classList.contains('nav-link')){
         clickTarget.classList.add('active');
         activeBtn.classList.remove('active');
+        let mobile = document.querySelector("mobile-menu")
+        if (!mobile.classList.contains('hide')){
+            mobile.classList.add('hide');
+        }
     }
 })
 let classLink = '.main-link';
@@ -39,4 +43,15 @@ window.onscroll = function(){
         activeButton.classList.remove('active');
     }
 };
+document.querySelector('.mobile-button').addEventListener("click", function(event){
+    document.querySelector('.mobile-menu').classList.toggle("hide");
+    let mobilemenu = document.querySelector('.mobile-menu')
+    let body = document.querySelector('body')
+    if (!body.classList.contains('off-scroll') && !mobilemenu.classList.contains('hide')){
+        body.classList.add('off-scroll')
+    }
+    else {
+        body.classList.remove('off-scroll')
+    }
+})
 
